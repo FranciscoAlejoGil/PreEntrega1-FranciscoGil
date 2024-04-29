924146aa0e203f77b4ea80d8f31b7903977adc50
Francisco Gil <gilfranciscoalejo@gmail.com>
2024-04-29 13:23:27 -0300

    Arreglando

--- src/components/BotonUp.jsx
+++ src/components/BotonUp.jsx
@@ -1,7 +1,7 @@
 import { ArrowBigUp } from 'lucide-react'
 function BotonUp () {
     return (
-        <a href="#top" class="fixed bottom-4 right-4 z-50 bg-zinc-700 hover:bg-zinc-400 hover:text-black text-white p-2 rounded-full shadow-md scroll-smooth ">
+        <a href="#top" className="fixed bottom-4 right-4 z-50 bg-zinc-700 hover:bg-zinc-400 hover:text-black text-white p-2 rounded-full shadow-md scroll-smooth ">
             <ArrowBigUp size={28} strokeWidth={1.5} />
         </a>
     )
